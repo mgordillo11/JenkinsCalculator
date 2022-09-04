24c92706bc86f8352ed980f9e0674b1113e95d11
Manuel <mgordillo11@outlook.com>
2022-09-03 21:52:04 -0600

    Add unique id function

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.UUID;
 
 class Calculator {
 
@@ -58,7 +59,7 @@ class Calculator {
     if int a = 16 then this method returns: 10000
      */
     String intToBinaryNumber(int n){
-        return null;
+        return Integer.toBinaryString(n);
     }
 
     /*
@@ -70,8 +71,10 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
+        UUID uuid = UUID.randomUUID();
+        String uuidAsString = n + uuid.toString();
 
-        return null;
+        return uuidAsString;
     }
 
 
